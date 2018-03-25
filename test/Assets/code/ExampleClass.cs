25e361bbd304975492fc0226c91d9e8d7f4b0155
Ernie1 <870949712@qq.com>
2018-03-25 23:06:54 +0800

    hide the test

--- test/Assets/code/ExampleClass.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ExampleClass : MonoBehaviour {
-	void res(){
-		Debug.Log(this.gameObject.name);
-	}
-}
