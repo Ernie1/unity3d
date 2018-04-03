9729c5678a8df8a5295aab4bc7f47882ebe3d545
Ernie1 <870949712@qq.com>
2018-04-03 17:36:52 +0800

    备份

--- hw2/牧师与魔鬼 第一版/Assets/Scripts/FirstController.cs
+++ hw2/牧师与魔鬼 第一版/Assets/Scripts/FirstController.cs
@@ -20,7 +20,8 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 	private int sailTo;
 	private int boatSeatIndex;
 	private Vector3 jumpFrom,jumpTo;
-	float startTime;
+	float dTime;
+	Vector3 speed, Gravity;
 
 	void Awake () {
 		boatPos = new Vector3[]{ new Vector3 (2, 0.1F, 0), new Vector3 (-2, 0.1F, 0) };
@@ -121,14 +122,14 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 		handleNum = -1;
 		//boat
 		if (objectClicked == boat) {
-			if (whereCanSit () != 0) {				
+			if (someoneOnBoat ()) {				
 				handleNum = 0;
 				sailTo = 1 - whereBoat ();
 			} else
 				someObjectHandling = false;
 			return;
 		}
-		startTime = Time.time;
+//		startTime = Time.time;
 		jumpFrom = objectClicked.transform.position;
 		//priest
 		int index = whichPriest(objectClicked);
@@ -136,9 +137,11 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 			if (wherePriest (index) == 2) {
 				boatSeatIndex = whichSeatByPosition (objectClicked.transform.localPosition);
 				jumpTo = priestPos [index, whereBoat ()];
+				setJump ();
 				handleNum = 1;
 			} else if (wherePriest (index) == whereBoat () && whereCanSit () != 2) {
 				jumpTo = boat.transform.TransformPoint (onBoat [whereCanSit ()]);
+				setJump ();
 				handleNum = 2;
 			} else
 				someObjectHandling = false;
@@ -150,10 +153,12 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 			if (whereDevil (index) == 2) {
 				boatSeatIndex = whichSeatByPosition (objectClicked.transform.localPosition);
 				jumpTo = devilPos [index, whereBoat ()];
+				setJump ();
 				handleNum = 1;
 			}
 			else if (whereDevil (index) == whereBoat () && whereCanSit () != 2) {
 				jumpTo = boat.transform.TransformPoint (onBoat [whereCanSit ()]);
+				setJump ();
 				handleNum = 2;
 			} else
 				someObjectHandling = false;
@@ -186,6 +191,12 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 		return 2;
 	}
 
+	bool someoneOnBoat () {
+		if(boatSeat[0]&&boatSeat[1])
+			return false;
+		return true;
+	}
+
 	int whichSeatByPosition(Vector3 p){
 		if (p == onBoat [0])
 			return 0;
@@ -216,19 +227,68 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 		}
 	}
 
-	void jump() {
-		float journeyTime = 1.0F;
-		Vector3 center = (jumpFrom + jumpTo) * 0.5F;
-		center -= new Vector3 (0, 1, 0);
-		Vector3 riseRelCenter = -center;
-		Vector3 setRelCenter = jumpTo - center;
-		float fracComplete = (Time.time - startTime) / journeyTime;
-		objectClicked.transform.position = Vector3.Slerp (riseRelCenter, setRelCenter, fracComplete);
-		objectClicked.transform.position += center;
-		if (objectClicked.transform.position == jumpTo)
-			someObjectHandling = false;
+	void setJump(){
+		float ShotSpeed = 10; // 抛出的速度
+		float time = Vector3.Distance (jumpFrom, jumpTo) / ShotSpeed;
+		float g = -10;
+		dTime = 0;
+		speed = new Vector3 ((jumpTo.x - jumpFrom.x) / time, (jumpTo.y - jumpFrom.y) / time - 0.5f * g * time, (jumpTo.z - jumpFrom.z) / time);
+		Gravity = Vector3.zero;
 	}
 
+	void jump() {
+//		float journeyTime = 1.0F;
+//		Vector3 center = (jumpFrom + jumpTo) * 0.5F;
+//		center -= new Vector3 (0, 1, 0);
+//		Vector3 riseRelCenter = -center;
+//		Vector3 setRelCenter = jumpTo - center;
+//		float fracComplete = (Time.time - startTime) / journeyTime;
+//		objectClicked.transform.position = Vector3.Slerp (riseRelCenter, setRelCenter, fracComplete);
+//		objectClicked.transform.position += center;
+
+		float g = -10;
+		Gravity.y = g * (dTime += Time.fixedDeltaTime);// v=gt
+		objectClicked.transform.position += (speed + Gravity) * Time.fixedDeltaTime;//模拟位移
+//		Vector3 currentAngle = new Vector3();
+//		currentAngle.x = -Mathf.Atan((speed.y + Gravity.y) / speed.z) * Mathf.Rad2Deg;// 弧度转度：Mathf.Rad2Deg
+//		objectClicked.transform.eulerAngles = currentAngle;// 设置当前角度
+		if (objectClicked.transform.position.x - jumpTo.x < 0.5) {
+			objectClicked.transform.position = jumpTo;
+			someObjectHandling = false;
+		}
+	}
+
+	//0 lose 1 win 2 -
+	bool checkGame () {
+		int priestHere = 0, priestThere = 0, devilHere = 0, devilThere = 0;
+		for(int i=0;i<3;++i){
+			if (wherePriest (i) == 0)
+				++priestHere;
+			else if (wherePriest (i) == 1)
+				++priestThere;
+			else if (whereBoat () != 2) {
+				if (whereBoat () == 0)
+					++priestHere;
+				else
+					++priestThere;
+			}
+			if (whereDevil (i) == 0)
+				++devilHere;
+			else if (whereDevil (i) == 1)
+				++devilThere;
+			else if (whereBoat () != 2) {
+				if (whereBoat () == 0)
+					++devilHere;
+				else
+					++devilThere;
+			}
+		}
+		if ((priestHere > 0 && priestHere < devilHere) || (priestThere > 0 && priestThere < devilThere))
+			return 0;
+		if (priestThere == 3 && devilThere == 3)
+			return 1;
+		return 2;
+	}
 
 	// Update is called once per frame
 	void Update () {
@@ -242,5 +302,7 @@ public class FirstController : MonoBehaviour, ISceneController, IUserAction {
 			else if (handleNum == 2)
 				aboard ();
 		}
+		if (checkGame () == 0)
+			Debug.Log ("lose");
 	}
 }
