452ee7a771d28fe93057b3b193e2863464ccd37a
Ernie1 <870949712@qq.com>
2018-06-19 20:27:17 +0800

    somemodify

--- hw9/Assets/_Completed-Assets/Scripts/Tank/TankAI.cs
+++ hw9/Assets/_Completed-Assets/Scripts/Tank/TankAI.cs
@@ -2,41 +2,33 @@ using System;
 using UnityEngine;
 namespace Complete
 {
-//    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
-//    [RequireComponent(typeof (ThirdPersonCharacter))]
+    [RequireComponent(typeof (UnityEngine.AI.NavMeshAgent))]
     public class TankAI : MonoBehaviour
     {
-        public UnityEngine.AI.NavMeshAgent agent { get; private set; }             // the navmesh agent required for the path finding
-		public TankCharacter character { get; private set; } // the character we are controlling
-        public Transform target;                                    // target to aim for
+        public UnityEngine.AI.NavMeshAgent agent { get; private set; }
+        public Transform target;
 		public float angle = 60f;
 		
 		private float countTime = 0;
         private void Start()
         {
-            // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
-			character = GetComponent<TankCharacter>();
-
 	        agent.updateRotation = true;
 	        agent.updatePosition = true;
         }
 
-
         private void Update()
         {
+			// 距离与40的比例
 			float ratio = (target.transform.position - transform.position).magnitude / 40;
+			// 导航速度与距离线性正相关
 			agent.speed = 2f * ratio + 1.5f;
+			// 导航角速度与距离线性负相关
 			agent.angularSpeed = 120f - 60f * ratio;
 			if (target != null)
                 agent.SetDestination(target.position);
-            if (agent.remainingDistance > agent.stoppingDistance)
-                character.Move(agent.desiredVelocity);
-            else
-				character.Move(Vector3.zero);
-		
+			// 玩家在正前方时，以0.2秒一次的频率发射子弹
 			if (countTime >= 0.2f) {
-				// 发射
 				Ray ray = new Ray (transform.position, transform.forward);
 				RaycastHit hit;  
 				if (Physics.Raycast (ray, out hit, Mathf.Infinity)) { 
@@ -47,11 +39,5 @@ namespace Complete
 			}
 			countTime += Time.deltaTime;
 		}
-
-
-        public void SetTarget(Transform target)
-        {
-            this.target = target;
-        }
     }
 }
--- hw9/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
+++ hw9/Assets/_Completed-Assets/Scripts/Tank/TankShooting.cs
@@ -45,13 +45,15 @@ namespace Complete
             m_AimSlider.value = m_MinLaunchForce;
 
             // If the max force has been exceeded and the shell hasn't yet been launched...
-			if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired) {
+			if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+			{
 				// ... use the max force and launch the shell.
 				m_CurrentLaunchForce = m_MaxLaunchForce;
 				Fire ();
 			}
             // Otherwise, if the fire button has just started being pressed...
-			else if (Input.GetButtonDown (m_FireButton)) {
+			else if (Input.GetButtonDown (m_FireButton))
+			{
 				// ... reset the fired flag and reset the launch force.
 				m_Fired = false;
 				m_CurrentLaunchForce = m_MinLaunchForce;
@@ -61,20 +63,23 @@ namespace Complete
 				m_ShootingAudio.Play ();
 			}
             // Otherwise, if the fire button is being held and the shell hasn't been launched yet...
-			else if (Input.GetButton (m_FireButton) && !m_Fired) {
+			else if (Input.GetButton (m_FireButton) && !m_Fired)
+			{
 				// Increment the launch force and update the slider.
 				m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
 
 				m_AimSlider.value = m_CurrentLaunchForce;
 			}
             // Otherwise, if the fire button is released and the shell hasn't been launched yet...
-			else if (Input.GetButtonUp (m_FireButton) && !m_Fired) {
+			else if (Input.GetButtonUp (m_FireButton) && !m_Fired)
+			{
 				// ... launch the shell.
 				Fire ();
 			}
         }
 
-		public void MyShoot () {
+		public void MyShoot ()
+		{
 			m_Fired = false;
 			m_CurrentLaunchForce = m_MinLaunchForce;
 			m_ShootingAudio.clip = m_ChargingClip;
